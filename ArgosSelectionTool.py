bcbc3dc188e413b967fc95674e8dcb42f6b26e9e
dashiellrs <dr343@duke.edu>
2026-09-09 12:48:38 -0400

    Add boolean condition for in/out of box

--- ArgosSelectionTool.py
+++ ArgosSelectionTool.py
@@ -17,7 +17,7 @@ the_box = {
     'y_max' : -75.00
 }
 
-# Copy and paste a line of data as the lineString variable value
+# Copy and paste a line of data as the linString variable value
 lineString = '10154641232,true,2019-05-14 13:37:52.000,' \
 '-75.49356999999998,34.86216,,0.0,-127.0,4.0167976787E8,5141.0,' \
 '424,"40",34.86216,34.86216,"0",-75.49356999999998,' \
@@ -29,13 +29,19 @@ lineString = '10154641232,true,2019-05-14 13:37:52.000,' \
 line_data = lineString.split(sep=',')
 
 # Assign variables to specific items in the list
-event_id = line_data[0]  # Argos tracking event ID ("event-id")
-timestamp = line_data[2] # Observation date ("timestamp")
-lat = line_data[4]       # Observation latitude ("location-lat")
-lon = line_data[3]       # Observation longitude ("location-lon")
-lc  = line_data[14]      # Observation location class ("argos:lc")
-tag_id = line_data[34]     # Tag identifier ("tag-local-identifier")
+event_id = line_data[0]     # Argos tracking event ID ("event-id")
+timestamp = line_data[2]    # Observation date ("timestamp")
+lat = float(line_data[4])   # Observation latitude ("location-lat")
+lon = float(line_data[3])   # Observation longitude ("location-lon")
+lc  = line_data[14]         # Observation location class ("argos:lc")
+tag_id = line_data[34]      # Tag identifier ("tag-local-identifier")
 
-# Print information to the use
-print (f"Record {event_id} indicates {tag_id} was seen at {lat}N and \
-       {lon}W on {timestamp}")
+# Evaluate latitude and longitude conditions
+lat_condition = the_box['y_min'] < lat < the_box['y_max']
+lon_condition = the_box['x_min'] < lon < the_box['x_max']
+
+# Report hte status of the points
+if lat_condition & lon_condition:
+    print(f'Record {event_id}: {tag_id} was IN the box at {timestamp}')
+else:
+    print(f'Record {event_id}: {tag_id} was NOT in the box at {timestamp}')
